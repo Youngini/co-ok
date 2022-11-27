e896a6d6ec54c34fa2ef82188b300892f62a8e7a
Oh-Huiseok <rlatotquf45@naver.com>
2022-11-27 15:44:19 +0900

    reivse, comments

--- Evaluation/evaluation.py
+++ Evaluation/evaluation.py
@@ -1,21 +1,25 @@
 #상품점수 score
 #상세평가 detailed_eval
+#import Product
+#from datetime import date, timedelta
 
 class Evaluation:
-	def __init__(self, score, detailed_eval):
-		self.score = score
-		self.detailed_eval = detailed_eval
+    #prod = Product()
 
-	def setScore(self, score):
-		self.score = score
+    def __init__(self, score, detailed_eval):
+        self.__score = score
+        self.__detailed_eval = detailed_eval
 
-	def setDetailed_eval(self, detailed_eval):
-		self.detailed_eval = detailed_eval
+    def setScore(self, score):
+        self.score = score
 
-	def getScore(self):
-		return self.score
+    def setDetailed_eval(self, detailed_eval):
+        self.detailed_eval = detailed_eval
 
-	def getDetailed_eval(self):
-		return self.detailed_eval
+    def getScore(self):
+        return self.score
+
+    def getDetailed_eval(self):
+        return self.detailed_eval
 
     
--- Product/product.py
+++ Product/product.py
@@ -7,61 +7,76 @@
 # 상품사진 product_pict
 # 카테고리 category
 
+from datetime import date, timedelta
+#import Evaluation
+
 class Product:
-    def __init__(self, product_name, cost, discount_per_person, min_group_num, max_group_num, apply_deadline, product_pict, category):
-        self.product_name = product_name
-        self.cost = cost
-        self.discount_per_person = discount_per_person
-        self.min_group_num = min_group_num
-        self.max_group_num = max_group_num
-        self.apply_deadline = apply_deadline
-        self.product_pict = product_pict
-        self.category = category
+	#seller = seller()
+	def __init__(self, product_name="", cost=1, discount_per_person=0, min_group_num=10,max_group_num=10, apply_deadline=date.today(), product_pict="", category=""):
+		self.__product_name = product_name
+		self.__cost = cost
+		self.__discount_per_person = discount_per_person
+		self.__min_group_num = min_group_num
+		self.__max_group_num = max_group_num
+		self.__apply_deadline = apply_deadline
+		self.__product_pict = product_pict
+		self.__category = category
 
-    def setProduct_name(self, product_name):
-        self.product_name = product_name
+	def setProduct_name(self, product_name):
+		self.product_name = product_name
 
-    def setCost(self, cost):
-        self.cost = cost
+	def setCost(self, cost):
+		if cost >= 1 and cost <= 999999999:
+			self.cost = cost
 
-    def setDiscount_per_person(self, discount_per_person):
-        self.discount_per_person = discount_per_person
+	def setDiscount_per_person(self, discount_per_person):
+		self.discount_per_person = discount_per_person
 
-    def setMin_group_num(self, min_group_num):
-        self.min_group_num = min_group_num
+	def setMin_group_num(self, min_group_num):
+		if min_group_num >= 10:
+			self.min_group_num = min_group_num
+	
+	def setMax_group_num(self, max_group_num):
+		if max_group_num <= 500:
+			self.max_group_num = max_group_num
 
-    def setMax_group_num(self, max_group_num):
-        self.max_group_num = max_group_num
+	def setApply_deadline(self, apply_deadline):
+		self.apply_deadline = apply_deadline
 
-    def setApply_deadline(self, apply_deadline):
-        self.apply_deadline = apply_deadline
+	def setProduct_pict(self, product_pict):
+		self.product_pict = product_pict
+	
+	def setCategory(self, category):
+		self.category = category
+	
+	def getProduct_name(self):
+		return self.product_name
 
-    def setProduct_pict(self, product_pict):
-        self.product_pict = product_pict
+	def getCost(self):
+		return self.cost
 
-    def setCategory(self, category):
-        self.category = category
+	def getDiscount_per_person(self):
+		return self.discount_per_person
 
-    def getProduct_name(self):
-        return self.product_name
+	def getMin_group_num(self):
+		return self.min_group_num
 
-    def getCost(self):
-        return self.cost
+	def getMax_group_num(self):
+		return self.max_group_num
 
-    def getDiscount_per_person(self):
-        return self.discount_per_person
+	def getApply_deadline(self):
+		return self.apply_deadline
 
-    def getMin_group_num(self):
-        return self.min_group_num
+	def getProduct_pict(self):
+		return self.product_pict
 
-    def getMax_group_num(self):
-        return self.max_group_num
+	def getCategory(self):
+		return self.category
 
-    def getApply_deadline(self):
-        return self.apply_deadline
-
-    def getProduct_pict(self):
-        return self.product_pict
-
-    def getCategory(self):
-        return self.category
+    # def __getDiscounted(self):
+	# def setDetailed_info(self, )
+	# def getMaxprice(self)
+	# def getMinprice(self)
+	# def getGroup()
+	# 최소가격이 원가의 50%이하일때 판매자한테 경고
+	# 할인공식:(100-인당할인률)/100 ** 구매인원 
