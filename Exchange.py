a5b7690c4a00daa070596bd4987d442ae469fcc2
ky107601 <104256486+ky107601@users.noreply.github.com>
2022-11-28 21:55:19 +0900

    Update Exchange.py

--- Exchange.py
+++ Exchange.py
@@ -8,22 +8,22 @@ class Exchange:
         self.faulty_img = faulty_img
         self.order_date = order_date
 
-    def setReason(self, reason):
+    def set_reason(self, reason):
         self.reason = reason
 
-    def setFaultyImg(self, faulty_img):
+    def set_faulty_img(self, faulty_img):
         self.faulty_img = faulty_img
 
-    def setOrderDate(self):
+    def set_order_date(self):
         self.order_date = order_date
 
-    def getReason(self):
+    def get_reason(self):
         return self.reason
 
-    def getFaultyImg(self):
+    def get_faulty_img(self):
         return self.faulty_img
 
-    def getOrderDate(self):
+    def get_order_date(self):
         return self.order_date
     
 
